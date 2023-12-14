f88f5387eaa5852051eb2099f0a80aca674b988b
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-12-13 21:57:18 -0500

    add sample file

---
+++ src/components/sample.sv
@@ -0,0 +1,5 @@
+/* Sample file to compile */
+
+module sample (input logic a, output logic b);
+    assign b = a;
+endmodule
---
+++ src/components/tests/test_sample.sv
@@ -0,0 +1,16 @@
+/* Sample file to test */
+
+module test_sample ();
+
+    logic in_1, out_1; 
+
+    sample sample_obj (.a(in_1), .b(out_1));
+
+    initial begin
+
+        in_1 = 1;
+        assert(in_1 == out_1)
+
+        $finish;
+    end
+endmodule
