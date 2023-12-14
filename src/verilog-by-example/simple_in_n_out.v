b19e85660d6cbc99677381967cd13dc415942409
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-12-13 19:12:03 -0500

    add settings

---
+++ src/verilog-by-example/simple_in_n_out.v
@@ -0,0 +1,16 @@
+/*
+ * Header Information
+ *
+ */
+
+module simple_in_n_out (in_1,
+                        in_2,
+                        in_3);
+    
+    /* Port definition */
+    input in_1;
+    input in_2;
+    input in_3;
+    
+    
+endmodule
