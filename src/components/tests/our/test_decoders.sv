77d2b4690a4a516b9fb0ebe551d275f961a81f5e
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-12-14 19:24:05 -0500

    implement 2_to_4

--- src/components/tests/our/test_decoders.sv
+++ src/components/tests/our/test_decoders.sv
@@ -9,7 +9,12 @@ module test_mux ();
     logic in_1_to_2;
     logic [1:0] out_1_to_2;
 
+    logic [1:0] in_2_to_4;
+    logic [3:0] out_2_to_4;
+
     decoder_1_to_2 d1to2(.in(in_1_to_2), .ena(enabled), .out(out_1_to_2));
+    decoder_2_to_4 d2to4(.in(in_2_to_4), .ena(enabled), .out(out_2_to_4));
+
 
     function void print_break;
         begin
@@ -23,6 +28,7 @@ module test_mux ();
             for(int is_enabled = 0; is_enabled <= 1; is_enabled++) begin
             /* Seed random input to the decoder */
             in_1_to_2 = $urandom();
+            in_2_to_4 = $urandom();
             
             /* Set the enabled state */
             enabled = is_enabled[0];
@@ -38,7 +44,16 @@ module test_mux ();
              end else begin
                 assert(out_1_to_2 == (1 << in_1_to_2)) else $fatal;
              end
+            print_break();
 
+            /* Test 2-to-4 decoder */
+            print_break();
+             $display("[2-to-4 decoder test]: [is_enabled: %0d] [in: %0d] [out: %0d]", is_enabled, in_2_to_4, out_2_to_4);
+             if(is_enabled == 0) begin
+                assert(out_1_to_2 == 0) else $fatal;
+             end else begin
+                assert(out_2_to_4 == (1 << in_2_to_4)) else $fatal;
+             end
             print_break();
             
 
