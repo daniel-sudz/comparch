3e70ff6c1a313970f36cca4b8fb6bc37cfd8eb6b
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-12-16 21:02:26 -0500

    compare_lt failing

---
+++ src/components/src/comparators/comparator_lt.sv
@@ -0,0 +1,50 @@
+`timescale 1ns/1ps
+`default_nettype none
+
+/*  ----- Implementation Constraints  -----
+ *  Using only *structural* combinational logic (or instantiated modules that 
+ *  only consist of structural combination logic), make a module that outputs 
+ *  high if a < b.
+ *
+ *  If you use other modules, make sure to update the sources in the Makefile 
+ *  appropriately! 
+*/
+
+module comparator_lt(a, b, out);
+
+    parameter N = 32;
+
+     /* ----- Inputs  ----- */
+    input wire signed [N-1:0] a, b;
+
+    /* ----- Outputs  ----- */
+    output logic out;
+
+
+    /* ----------- Wires ----------- */
+    wire a_eq_b;
+    logic b_already_bigger [N-1:0];
+    logic ok [N-1:0];
+
+
+    /* ----- Design  ----- */
+    comparator_eq check_eq(.a(b), .b(b), .out(a_eq_b));
+
+    /* Base case */
+    assign b_already_bigger[N-1] = (~b[N-1] & a[N-1]);
+    assign ok[N-1] = (a[N-1] | ~b[N-1]);
+
+    /* Compare bit-by-bit, carrying through the previous position's bit equality check */
+    genvar i;
+    generate 
+        for(i=N-2;i>=0;i--) begin
+            assign b_already_bigger[i] = b_already_bigger[i+1] | (b[i] & ~a[i]);
+            assign ok[i] = ok[i+1] & (b_already_bigger[i] | (~a[i] | b[i]));
+        end
+    endgenerate
+
+    /* First position of ok holds the entire less than or equal check */
+    assign out = (ok[0] & ~a_eq_b);
+
+
+endmodule
--- src/components/tests/our/test_comparators.sv
+++ src/components/tests/our/test_comparators.sv
@@ -7,14 +7,17 @@ module test_comparators ();
 
     parameter N = 32;
 
-    logic [N-1:0] input_a;
-    logic [N-1:0] input_b;
+    logic signed [N-1:0] input_a;
+    logic signed [N-1:0] input_b;
 
     logic compare_eq_out;
     logic expected_compare_eq_out;
 
-    comparator_eq ceq(.a(input_a), .b(input_b), .out(compare_eq_out));
+    logic compare_lt_out;
+    logic expected_compare_lt_out;
 
+    comparator_eq ceq(.a(input_a), .b(input_b), .out(compare_eq_out));
+    comparator_lt clt(.a(input_a), .b(input_b), .out(compare_lt_out));
 
 
     function void print_break;
@@ -26,22 +29,40 @@ module test_comparators ();
 
     initial begin
         for(int test_cases=0; test_cases<1000; test_cases++) begin
-            /* Seed random input */
 
             /* Test compare_eq */
+            print_break();
             input_a = $urandom();
             input_b = $urandom();
             expected_compare_eq_out = (input_a == input_b);
             #10;
             $display("[compare_eq]: [a: %0d], [b: %0d], [out: %0d] [expected_out: %0d]", input_a, input_b, compare_eq_out, expected_compare_eq_out);
-            assert(compare_eq_out == (input_a == input_b)) else $fatal;
+            assert(compare_eq_out == expected_compare_eq_out) else $fatal;
             
             input_a = $urandom();
             input_b = input_a;
             expected_compare_eq_out = (input_a == input_b);
             #10;
             $display("[compare_eq]: [a: %0d], [b: %0d], [out: %0d] [expected_out: %0d]", input_a, input_b, compare_eq_out, expected_compare_eq_out);
-            assert(compare_eq_out == (input_a == input_b)) else $fatal;
+            assert(compare_eq_out == expected_compare_eq_out) else $fatal;
+            print_break();
+
+            /* Test compare_lt */
+            print_break();
+            input_a = $urandom();
+            input_b = $urandom();
+            expected_compare_lt_out = (input_a < input_b);
+            #10;
+            $display("[compare_lt]: [a: %0d], [b: %0d], [out: %0d] [expected_out: %0d]", input_a, input_b, compare_lt_out, expected_compare_lt_out);
+            assert(compare_lt_out == expected_compare_lt_out) else $fatal;
+            
+            input_a = $urandom();
+            input_b = input_a;
+            expected_compare_eq_out = (input_a < input_b);
+            #10;
+            $display("[compare_lt]: [a: %0d], [b: %0d], [out: %0d] [expected_out: %0d]", input_a, input_b, compare_lt_out, expected_compare_lt_out);
+            assert(compare_lt_out == expected_compare_lt_out) else $fatal;
+            print_break();
             
             $display("[PASS RANDOM TEST] iteration #%d", test_cases);
         end
