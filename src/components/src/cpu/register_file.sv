b7c77b3f1fb6e515c153042672cca4978c9d16c3
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-12-31 14:38:38 -0500

    add print state

--- src/components/src/cpu/register_file.sv
+++ src/components/src/cpu/register_file.sv
@@ -63,4 +63,25 @@ module register_file(rst, clk, wr_ena, wr_addr, wr_data, rd_addr0, rd_data0, rd_
     always_comb rd_data0 = xn[rd_addr0];
     always_comb rd_data1 = xn[rd_addr1];
 
+    function void print_state();
+        string reg_idx [31:0] = '{
+            "x00","x01","x02","x03","x04","x05","x06","x07","x08","x09",
+            "x10","x11","x12","x13","x14","x15","x16","x17","x18","x19",
+            "x20","x21","x22","x23","x24","x25","x26","x27","x28","x29",
+            "x30","x31"};
+        string reg_type [31:0] = '{
+            "zero","ra","sp","gp","tp","t0","t1","t2","s0","s1",
+            "a0","a1","a2","a3","a4","a5","a6","a7","s2","s3",
+            "s4","s5","s6","s7","s8","s9","s10","s11","t3","t4",
+            "t5","t6"};
+
+        $display("|---------------------------------------|");
+        $display("| Register File State                   |");
+        $display("|---------------------------------------|");
+        for(integer i=0;i<32;i++) begin
+            $display("| %12s = 0x%8h (%10d)|", reg_idx[i], reg_type[i], xn[i], xn[i]);
+        end
+        $display("|---------------------------------------|");
+        endfunction 
+
 endmodule
