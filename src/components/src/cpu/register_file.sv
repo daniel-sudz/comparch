9cba4d7af727c876e365a29de595caaa45ba7364
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-12-31 19:59:09 -0500

    cpu simulator can run with dependencies (#9)
    * add provided r32_common file
    
    * add more types
    
    * add more templates
    
    * add print state
    
    * fix print function
    
    * convert to task
    
    * more provided files
    
    * fix compile errors
    
    * working make file
    
    * cpu simulator can run with dependencies

--- src/components/src/cpu/register_file.sv
+++ src/components/src/cpu/register_file.sv
@@ -63,4 +63,44 @@ module register_file(rst, clk, wr_ena, wr_addr, wr_data, rd_addr0, rd_data0, rd_
     always_comb rd_data0 = xn[rd_addr0];
     always_comb rd_data1 = xn[rd_addr1];
 
+
+    task print_state;
+        $display("|---------------------------------------|");
+        $display("| Register File State :)                |");
+        $display("|---------------------------------------|");
+        $display("| %12s = 0x%8h (%10d)|", "x00, zero", xn[0], xn[0]);
+        $display("| %12s = 0x%8h (%10d)|", "x01, ra", xn[1], xn[1]);
+        $display("| %12s = 0x%8h (%10d)|", "x02, sp", xn[2], xn[2]);
+        $display("| %12s = 0x%8h (%10d)|", "x03, gp", xn[3], xn[3]);
+        $display("| %12s = 0x%8h (%10d)|", "x04, tp", xn[4], xn[4]);
+        $display("| %12s = 0x%8h (%10d)|", "x05, t0", xn[5], xn[5]);
+        $display("| %12s = 0x%8h (%10d)|", "x06, t1", xn[6], xn[6]);
+        $display("| %12s = 0x%8h (%10d)|", "x07, t2", xn[7], xn[7]);
+        $display("| %12s = 0x%8h (%10d)|", "x08, s0", xn[8], xn[8]);
+        $display("| %12s = 0x%8h (%10d)|", "x09, s1", xn[9], xn[9]);
+        $display("| %12s = 0x%8h (%10d)|", "x10, a0", xn[10], xn[10]);
+        $display("| %12s = 0x%8h (%10d)|", "x11, a1", xn[11], xn[11]);
+        $display("| %12s = 0x%8h (%10d)|", "x12, a2", xn[12], xn[12]);
+        $display("| %12s = 0x%8h (%10d)|", "x13, a3", xn[13], xn[13]);
+        $display("| %12s = 0x%8h (%10d)|", "x14, a4", xn[14], xn[14]);
+        $display("| %12s = 0x%8h (%10d)|", "x15, a5", xn[15], xn[15]);
+        $display("| %12s = 0x%8h (%10d)|", "x16, a6", xn[16], xn[16]);
+        $display("| %12s = 0x%8h (%10d)|", "x17, a7", xn[17], xn[17]);
+        $display("| %12s = 0x%8h (%10d)|", "x18, s2", xn[18], xn[18]); 
+        $display("| %12s = 0x%8h (%10d)|", "x19, s3", xn[19], xn[19]); 
+        $display("| %12s = 0x%8h (%10d)|", "x20, s4", xn[20], xn[20]); 
+        $display("| %12s = 0x%8h (%10d)|", "x21, s5", xn[21], xn[21]); 
+        $display("| %12s = 0x%8h (%10d)|", "x22, s6", xn[22], xn[22]); 
+        $display("| %12s = 0x%8h (%10d)|", "x23, s7", xn[23], xn[23]); 
+        $display("| %12s = 0x%8h (%10d)|", "x24, s8", xn[24], xn[24]); 
+        $display("| %12s = 0x%8h (%10d)|", "x25, s9", xn[25], xn[25]); 
+        $display("| %12s = 0x%8h (%10d)|", "x26, s10", xn[26], xn[26]); 
+        $display("| %12s = 0x%8h (%10d)|", "x27, s11", xn[27], xn[27]); 
+        $display("| %12s = 0x%8h (%10d)|", "x28, t3", xn[28], xn[28]); 
+        $display("| %12s = 0x%8h (%10d)|", "x29, t4", xn[29], xn[29]); 
+        $display("| %12s = 0x%8h (%10d)|", "x30, t5", xn[30], xn[30]); 
+        $display("| %12s = 0x%8h (%10d)|", "x31, t6", xn[31], xn[31]); 
+        $display("|---------------------------------------|");
+    endtask // print_state
+
 endmodule
---
+++ src/components/src/cpu/rv32i_multicycle_core.sv
@@ -0,0 +1,109 @@
+`timescale 1ns/1ps
+`default_nettype none
+
+`include "alu_types.sv"
+`include "memory_access.sv"
+`include "memory_exceptions.sv"
+`include "rv32_common.sv"
+`include "memory_map.sv" 
+
+module rv32i_multicycle_core(
+    clk, rst, ena,
+    mem_addr, mem_rd_data, mem_wr_data, mem_wr_ena,
+    mem_access, mem_exception,
+    PC, instructions_completed, instruction_done
+    );
+    
+    enum logic [1:0] {S_FETCH} state;
+
+    parameter [31:0] PC_START_ADDRESS = {MMU_BANK_INST, 28'h0};
+
+
+    // Standard control signals.
+    input  wire clk, rst, ena; // <- worry about implementing the ena signal last.
+    output logic instruction_done; // Should be high for one clock cycle when finishing an instruction (e.g. during the writeback state).
+
+    // Memory interface.
+    output logic [31:0] mem_addr, mem_wr_data;
+    input   wire [31:0] mem_rd_data;
+    output mem_access_t mem_access;
+    input mem_exception_mask_t mem_exception;
+    output logic mem_wr_ena;
+
+    // Program Counter
+    output wire [31:0] PC;
+    output logic [31:0] instructions_completed; // TODO(student) - increment this by one whenever an instruction is complete.
+    wire [31:0] PC_old;
+    logic PC_ena, PC_old_ena;
+    logic [31:0] PC_next;
+
+    // Control Signals
+    // Decoder
+    logic [6:0] op;
+    logic [2:0] funct3;
+    logic [6:0] funct7;
+    logic rtype, itype, ltype, stype, btype, jtype;
+    enum logic [2:0] {IMM_SRC_ITYPE, IMM_SRC_STYPE, IMM_SRC_BTYPE, IMM_SRC_JTYPE, IMM_SRC_UTYPE} immediate_src;
+    logic [31:0] extended_immediate;
+
+    // R-file Control Signals
+    logic [4:0] rd, rs1, rs2;
+    wire [31:0] reg_data1, reg_data2;
+    logic reg_write;
+    logic [31:0] rfile_wr_data;
+    wire [31:0] reg_A, reg_B;
+
+    // ALU Control Signals
+    enum logic [1:0] {ALU_SRC_A_PC, ALU_SRC_A_RF, ALU_SRC_A_OLD_PC, ALU_SRC_A_ZERO} 
+    alu_src_a;
+    enum logic [1:0] {ALU_SRC_B_RF, ALU_SRC_B_IMM, ALU_SRC_B_4, ALU_SRC_B_ZERO} 
+    alu_src_b;
+    logic [31:0] src_a, src_b;
+    wire [31:0] alu_result;
+    alu_control_t alu_control, ri_alu_control;
+    wire overflow;
+    wire zero;
+    wire equal;
+
+    // Non-architectural Register Signals
+    logic IR_write;
+    wire [31:0] IR; // Instruction Register (current instruction)
+    logic ALU_ena;
+    wire [31:0] alu_last; // Not a descriptive name, but this is what it's called in the text.
+    logic mem_data_ena;
+    wire [31:0] mem_data;
+    enum logic {MEM_SRC_PC, MEM_SRC_RESULT} mem_src;
+    enum logic [1:0] {RESULT_SRC_ALU, RESULT_SRC_MEM_DATA, RESULT_SRC_ALU_LAST} result_src; 
+    logic [31:0] result;
+
+    // Program Counter Register
+    register #(.N(32), .RESET_VALUE(PC_START_ADDRESS)) PC_REGISTER (
+    .clk(clk), .rst(rst), .ena(PC_ena), .d(PC_next), .q(PC)
+    );
+
+    // Register file
+    register_file REGISTER_FILE(
+    .clk(clk), .rst(rst),
+    .wr_ena(reg_write), .wr_addr(rd), .wr_data(rfile_wr_data),
+    .rd_addr0(rs1), .rd_addr1(rs2),
+    .rd_data0(reg_data1), .rd_data1(reg_data2)
+    );
+
+    task print_rfile();
+        REGISTER_FILE.print_state();
+    endtask
+
+    // Non-architecture register: save register read data for future cycles.
+    register #(.N(32)) REGISTER_A (.clk(clk), .rst(rst), .ena(1'b1), .d(reg_data1), .q(reg_A));
+    register #(.N(32)) REGISTER_B (.clk(clk), .rst(rst), .ena(1'b1), .d(reg_data2), .q(reg_B));
+    always_comb mem_wr_data = reg_B; // RISC-V always stores data from this location.
+
+    // ALU and related control signals - use the behavioral one if you need to.
+    alu ALU (
+    .a(src_a), .b(src_b), .result(alu_result),
+    .control(alu_control),
+    .overflow(overflow), .zero(zero), .equal(equal)
+    );
+
+
+endmodule
