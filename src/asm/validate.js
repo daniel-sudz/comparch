5316c4e645828569e670a59be22833c2624d5a75
Daniel Sudzilouski <daniel@sudzilouski.com>
2024-01-09 14:51:38 -0500

    regression tests look good

--- src/asm/validate.js
+++ src/asm/validate.js
@@ -13,18 +13,25 @@ let expected = fs.readFileSync(
     path.resolve(__dirname, expected_file),  { encoding: 'utf8', flag: 'r' })
         .toString()
         .split("\n")
-        .filter(line => line.includes("TESTASSERTOUTPUT"));
+        .filter(line => line.includes("TESTASSERTOUTPUT") || line.includes("TESTASSERTIGNORE"));
 let actual = fs.readFileSync(
     path.resolve(__dirname, actual_file),  { encoding: 'utf8', flag: 'r' })
         .toString()
         .split("\n")
-        .filter(line => line.includes("TESTASSERTOUTPUT"));
+        .filter(line => line.includes("TESTASSERTOUTPUT") || line.includes("TESTASSERTIGNORE"));
 
 for(let i = 0; i < expected.length; i++) {
     console.log(expected[i])
     console.log(actual[i]);
 
-    if(expected[i] != actual[i]) {
+    if(expected[i].includes("TESTASSERTIGNORE")) {
+        // pass
+    }
+    else if(i == actual.length) {
+        console.error("[ERROR] FAILED TO ASSERT REGISTER FILE STATE NOT ENOUGH LINES!!!!!!!");
+    }
+    else if(expected[i] != actual[i]) {
+        console.error("[ERROR] FAILED TO ASSERT REGISTER FILE STATE !!!!!!!");
         process.exit(1);
     }
 
